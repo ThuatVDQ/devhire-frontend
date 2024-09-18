eef13cfc7a7c8e8f178f2823d9a3c7b8e6e7e349
ThuatVDQ <thuatvopro@gmail.com>
2024-09-18 18:39:25 +0700

    fix: card category, list category

--- src/components/ListCategory.vue
+++ src/components/ListCategory.vue
@@ -21,16 +21,16 @@ import CardCategory from './CardCategory.vue'
     <div
       class="grid xl:grid-cols-5 lg:grid-cols-4 md:grid-cols-3 sm:grid-cols-2 grid-cols-1 mt-8 gap-[30px]"
     >
-      <CardCategory />
-      <CardCategory />
-      <CardCategory />
-      <CardCategory />
-      <CardCategory />
-      <CardCategory />
-      <CardCategory />
-      <CardCategory />
-      <CardCategory />
-      <CardCategory />
+      <CardCategory category="Human Resource" slots="90" />
+      <CardCategory category="It & Networking" slots="90" />
+      <CardCategory category="Sales & Marketing" slots="90" />
+      <CardCategory category="Accounting" slots="90" />
+      <CardCategory category="Delivery Boy" slots="90" />
+      <CardCategory category="Data Science" slots="90" />
+      <CardCategory category="Project Manager" slots="90" />
+      <CardCategory category="Engineering" slots="90" />
+      <CardCategory category="Help Center" slots="90" />
+      <CardCategory category="Full Stack Developer" slots="90" />
     </div>
   </div>
 </template>
