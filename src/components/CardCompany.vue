13d603fbfaac317cfdc2ea7c11763995174d608b
ThuatVDQ <thuatvopro@gmail.com>
2024-09-24 16:32:03 +0700

    add: component companylist

--- src/components/CardCompany.vue
+++ src/components/CardCompany.vue
@@ -1,3 +1,12 @@
+<script setup>
+defineProps({
+  company: {
+    type: Object,
+    required: true
+  }
+})
+</script>
+
 <template>
   <div class="group relative p-6 rounded-md shadow dark:shadow-gray-700 mt-6">
     <div
@@ -6,13 +15,13 @@
       <img src="" alt="" />
     </div>
     <div class="mt-4">
-      <a href="" class="text-lg hover:text-emerald-600 font-semibold">Facebook</a>
-      <p class="text-slate-400 mt-2">Digital Marketing Solutions for Tomorrow</p>
+      <a href="" class="text-lg hover:text-emerald-600 font-semibold">{{ company.name }}</a>
+      <p class="text-slate-400 mt-2">{{ company.description }}</p>
     </div>
     <div class="mt-4 pt-4 border-t border-gray-100 dark:border-gray-700 flex justify-between">
       <span class="text-slate-400 flex items-center">
         <i class="pi pi-map-marker"></i>
-        Australia
+        {{ company.address }}
       </span>
       <span class="block font-semibold text-emerald-600"> 2 Jobs</span>
     </div>
