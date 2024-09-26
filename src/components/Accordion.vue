28af722b08515825e622358ebb7817b32fb4e8af
ThuatVDQ <thuatvopro@gmail.com>
2024-09-26 16:09:46 +0700

    add: company view

---
+++ src/components/Accordion.vue
@@ -0,0 +1,34 @@
+<script setup>
+const props = defineProps({
+  title: String,
+  description: String,
+  isOpen: Boolean
+})
+
+const emit = defineEmits(['toggle'])
+</script>
+
+<template>
+  <div class="relative shadow dark:shadow-gray-700 rounded-md overflow-hidden mt-4">
+    <h2 class="text-base font-medium">
+      <button
+        :class="{ 'text-emerald-500': isOpen, 'text-black': !isOpen }"
+        class="flex justify-between items-center p-5 w-full font-medium text-left bg-gray-50 dark:bg-slate-800"
+        @click="emit('toggle')"
+      >
+        <span>{{ title }}</span>
+        <i
+          :class="{ 'transform rotate-180': isOpen, 'transform rotate-0': !isOpen }"
+          class="pi pi-chevron-down size-4 shrink-0 transition-transform"
+        ></i>
+      </button>
+    </h2>
+    <div v-show="isOpen">
+      <div class="p-5">
+        <p class="text-slate-400 dark:text-gray-400">
+          {{ description }}
+        </p>
+      </div>
+    </div>
+  </div>
+</template>
--- src/router/index.js
+++ src/router/index.js
@@ -6,6 +6,7 @@ import LoginView from '@/views/LoginView.vue'
 import RegisterView from '@/views/RegisterView.vue'
 import ApplyJob from '@/views/ApplyJob.vue'
 import ProfileView from '@/views/ProfileView.vue'
+import CompanyView from '@/views/CompanyView.vue'
 const router = createRouter({
   history: createWebHistory(import.meta.env.BASE_URL),
   routes: [
@@ -43,6 +44,11 @@ const router = createRouter({
       path: '/profile/:id',
       name: 'profile',
       component: ProfileView
+    },
+    {
+      path: '/companies',
+      name: 'companies',
+      component: CompanyView
     }
   ],
   scrollBehavior(to, from, savedPosition) {
